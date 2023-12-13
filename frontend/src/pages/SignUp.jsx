f8e307b34de94c6e8e2faf98e8ec4dcae6d291b7
AKANKSHAJOSHI481 <joshiakanksha481@gmail.com>
2023-12-13 23:16:14 +0530

    sign up functionality done

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,29 +1,44 @@
-import {useState} from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link , useNavigate} from "react-router-dom";
 export default function SignUp() {
-  const [formData, setformData] =useState({})
-  const handleChange = (e) =>{
+  const [formData, setformData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
     setformData({
       ...formData,
-      [e.target.id] : e.target.value
-    })
-  }
-  console.log(formData)
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(formData);
 
-  const handleSubmit = async (e) =>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:3000/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        body : JSON.stringify(formData)
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:3000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
       }
-    })
-    const data = await res.json();
-    console.log(data)
-  }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in');
+    } catch (e) {
+      setLoading(false);
+      setError(e.message);
+    }
+  };
   return (
-    
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <form className="flex flex-col gap-4 " onSubmit={handleSubmit}>
@@ -48,8 +63,11 @@ export default function SignUp() {
           id="password"
           onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading ...." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -58,6 +76,7 @@ export default function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/user.route.js')
 const authRouter = require('./routes/auth.route.js')
@@ -12,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.listen(3000, ()=>{
     console.log('listening on port 3000')
 })
